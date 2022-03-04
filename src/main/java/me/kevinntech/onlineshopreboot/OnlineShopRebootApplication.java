a9f5c21846cf0c47764a1b87ab48b986d9d9fafa
kevinntech <kevinntech@nate.com>
2022-03-04 11:22:53 +0900

    Setup #1 - Spring Boot
    Spring Boot 2.5.10, Java 11 etc... 설정

---
+++ src/main/java/me/kevinntech/onlineshopreboot/OnlineShopRebootApplication.java
@@ -0,0 +1,13 @@
+package me.kevinntech.onlineshopreboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopRebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopRebootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kevinntech/onlineshopreboot/OnlineShopRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kevinntech.onlineshopreboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopRebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
