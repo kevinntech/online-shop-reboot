8f05c23e080b0b853b857df830696f66492013e0
kevinntech <kevinntech@nate.com>
2022-07-01 20:31:27 +0900

    Update - WebConfig
    WebConfig 파일에서 필요없는 import 문을 제거한다.

--- src/main/java/me/kevinntech/onlineshop/WebConfig.java
+++ src/main/java/me/kevinntech/onlineshop/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
