5fdb652ae34dfebf4dd456ec038956559db190d5
kevinntech <kevinntech@nate.com>
2022-07-02 18:50:26 +0900

    Implement #5 - OkResponse
    OK 응답 객체 작성

---
+++ src/main/java/me/kevinntech/onlineshop/base/dto/OkResponse.java
@@ -0,0 +1,26 @@
+package me.kevinntech.onlineshop.base.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OkResponse<T> {
+
+    private T data;
+
+    private LocalDateTime timestamp;
+
+    public OkResponse(T data) {
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public static <T> OkResponse of(T data) {
+        return new OkResponse(data);
+    }
+
+}
