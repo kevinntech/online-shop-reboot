ee347a23323e85d297e882e31b283ee8ea4fc9a0
kevinntech <kevinntech@nate.com>
2022-07-04 14:33:45 +0900

    Implement #7 - Stock Entity
    * 재고(Stock) 엔티티를 작성
    * 재고와 관련된 엔티티 간의 연관관계를 설정

--- src/main/java/me/kevinntech/onlineshop/domain/warehousing/Warehousing.java
+++ src/main/java/me/kevinntech/onlineshop/domain/warehousing/Warehousing.java
@@ -1,9 +1,8 @@
 package me.kevinntech.onlineshop.domain.warehousing;
 
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import me.kevinntech.onlineshop.domain.product.Product;
+import me.kevinntech.onlineshop.domain.stock.Stock;
 
 import javax.persistence.*;
 
@@ -25,10 +24,43 @@ public class Warehousing {
     @Column(name = "WAREHOUSING_ID")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
     @Column(name = "WAREHOUSING_PRICE")
     private long price;
 
     @Column(name = "WAREHOUSING_QUANTITY")
     private long quantity;
+
+    @Builder
+    public Warehousing(long price, long quantity) {
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    /*
+    * 연관관계 편의 메소드
+    * */
+
+    public void changeProduct(Product product) {
+        this.product = product;
+    }
+
+    public void changeStock(Stock stock) {
+        this.stock = stock;
+        stock.getWarehousingList().add(this);
+        stock.adjustStock(); // 재고 조정
+    }
+
+    public void establishRelationship(Product product, Stock stock) {
+        changeProduct(product);
+        changeStock(stock);
+    }
 
 }
